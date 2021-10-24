c3bc790cbec5ed864e5d85a01c0918d83ed5ba9b
P01010111 <85543897+P01010111@users.noreply.github.com>
2021-10-25 09:12:43 +1100

    Not sure what this means

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println(DigitSumChallenge.sumDigits(1447166));
+	    //System.out.println(DigitSumChallenge.sumDigits(24456));
+        NumberPalindrome.isPalindrome(505);
     }
 }
